dd0e4107e690bc6b3e676d9046a2431ffda8a184
khoirul92 <54443082+khoirul92@users.noreply.github.com>
2020-11-04 06:37:06 +0700

    Create stramingmovieonline.js

---
+++ stramingmovieonline.js
@@ -0,0 +1,26 @@
+var pages = [
+"https://stramingmovieonline.blogspot.com/2020/10/ini-yang-bikin-ibu-hamil-mual-dan.html",
+"https://stramingmovieonline.blogspot.com/2020/10/pria-yang-sudah-menikah-tidak-bisa.html",
+"https://stramingmovieonline.blogspot.com/2020/10/terus-perbaiki-diri-kita-agar-allah.html",
+"https://stramingmovieonline.blogspot.com/2020/10/tips-menghilangkan-bekas-jerawat-tanpa.html",
+"https://stramingmovieonline.blogspot.com/2020/10/tips-menghilangkan-bekas-jerawat-tanpa.html",
+"https://stramingmovieonline.blogspot.com/2020/10/funny-fails-celebrity-viral-on-youtube.html",
+"https://stramingmovieonline.blogspot.com/2020/10/court-strikes-down-womans-death.html",
+"https://stramingmovieonline.blogspot.com/2020/09/viral-meme-updates-on-week.html",
+"https://stramingmovieonline.blogspot.com/2020/08/hati-hati-ibu-menyusui-sambil-main-hp.html",
+"https://stramingmovieonline.blogspot.com/2020/07/stream-juice-wrlds-posthumous-album.html",
+"https://stramingmovieonline.blogspot.com/2020/06/appeals-court-says-order-allowing.html",
+"https://stramingmovieonline.blogspot.com/2020/06/beautiful-disaster.html",
+"https://stramingmovieonline.blogspot.com/2020/06/wallpaper-superhero-hd.html",
+"https://stramingmovieonline.blogspot.com/2020/06/wallpaper-iphone-hd.html",
+"https://stramingmovieonline.blogspot.com/2020/06/just-for-laugh.html",
+"https://stramingmovieonline.blogspot.com/2020/05/snowpiercer.html",
+"https://stramingmovieonline.blogspot.com/2020/03/klik-bait.html",
+"https://stramingmovieonline.blogspot.com/2020/03/dog-funny.html",
+
+
+];
+function randomPage() {
+return pages[Math.round(Math.random() * (pages.length - 1,2,3))];
+}
+location.href= randomPage();
