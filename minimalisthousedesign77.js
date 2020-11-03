9a888659bd6ebb497aa27c12dc9647c5e670cb5b
khoirul92 <54443082+khoirul92@users.noreply.github.com>
2020-11-04 06:28:25 +0700

    Create minimalisthousedesign77.js

---
+++ minimalisthousedesign77.js
@@ -0,0 +1,37 @@
+var pages = [
+"https://minimalisthousedesign77.blogspot.com/2020/10/10-manfaat-cengkeh-bagi-kecantikan-yang.html",
+"https://minimalisthousedesign77.blogspot.com/2020/10/ubahlah-5-sikap-ini-sebelum-kamu.html",
+"https://minimalisthousedesign77.blogspot.com/2020/10/tips-miliki-mata-jernih-secara-alami.html",
+"https://minimalisthousedesign77.blogspot.com/2020/10/jika-kau-melihat-istri-orang-lain.html",
+"https://minimalisthousedesign77.blogspot.com/2020/10/hindari-mengeluhkan-masalah-rumah.html",
+"https://minimalisthousedesign77.blogspot.com/2020/10/bahayakah-mengkonsumsi-telur-setiap.html",
+"https://minimalisthousedesign77.blogspot.com/2020/07/stream-juice-wrlds-posthumous-album.html",
+"https://minimalisthousedesign77.blogspot.com/2020/07/mary-kay-letourneau-teacher-jailed-in.html",
+"https://minimalisthousedesign77.blogspot.com/2018/10/how-can-i-finance-small-extension-and.html",
+"https://minimalisthousedesign77.blogspot.com/2018/10/blink-blue-cross-partner-for-travel.html",
+"https://minimalisthousedesign77.blogspot.com/2018/10/airline-travellers-demand-rights-and.html",
+"https://minimalisthousedesign77.blogspot.com/2018/10/insurers-recognized-as-some-of-atlantic.html",
+"https://minimalisthousedesign77.blogspot.com/2018/10/axa-sells-ukraine-insurance-business-to.html",
+"https://minimalisthousedesign77.blogspot.com/2018/10/car-insurance-shock-this-common-error.html",
+"https://minimalisthousedesign77.blogspot.com/2018/10/it-just-got-much-cheaper-to-visit-north.html",
+"https://minimalisthousedesign77.blogspot.com/2020/03/kamar-anak-dengan-tema-bajak-laut.html",
+"https://minimalisthousedesign77.blogspot.com/2020/03/kamar-anak-dengan-window-nook.html",
+"https://minimalisthousedesign77.blogspot.com/2020/03/kamar-anak-dengan-tema-mobil-balap.html",
+"https://minimalisthousedesign77.blogspot.com/2020/03/kamar-anak-dengan-mezanine.html",
+"https://minimalisthousedesign77.blogspot.com/2020/03/kamar-anak-dengan-tema-sirkus.html",
+"https://minimalisthousedesign77.blogspot.com/2020/03/kamar-anak-dengan-tema-hutan-yang-hijau.html",
+"https://minimalisthousedesign77.blogspot.com/2020/03/kamar-anak-dengan-tema-cinderella.html",
+"https://minimalisthousedesign77.blogspot.com/2020/03/kamar-anak-dengan-tema-hutan-yang-hijau.html",
+"https://minimalisthousedesign77.blogspot.com/2020/03/kamar-anak-untuk-3-orang-dengan-ranjang.html",
+"https://minimalisthousedesign77.blogspot.com/2020/03/kamar-anak-ala-putri-negeri-dongeng.html",
+"https://minimalisthousedesign77.blogspot.com/2020/03/kamar-anak-dengan-tema-lego.html",
+"https://minimalisthousedesign77.blogspot.com/2020/03/kamar-anak-dengan-tema-frozen.html",
+"https://minimalisthousedesign77.blogspot.com/2020/03/kamar-anak-dengan-tema-indian.html",
+"https://minimalisthousedesign77.blogspot.com/2020/03/kamar-anak-dengan-wahana-perosotan.html",
+"https://minimalisthousedesign77.blogspot.com/2020/06/funny-kungfu-cat.html",
+
+];
+function randomPage() {
+return pages[Math.round(Math.random() * (pages.length - 1,2,3))];
+}
+location.href= randomPage();
