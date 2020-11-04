40e168806f92f26dce6c0fde1cab2847a9aa8727
khoirul92 <54443082+khoirul92@users.noreply.github.com>
2020-11-04 22:28:27 +0700

    Create clickjenarsite.js

---
+++ clickjenarsite.js
@@ -0,0 +1,29 @@
+var pages = [
+"https://www.clickjenar.site/2020/04/funny-hijab-tiktok.html",
+"https://www.clickjenar.site/2020/04/fishing-shark.html",
+"https://www.clickjenar.site/2020/04/5-macam-penghalang-rezeki-yang-jarang.html",
+"https://www.clickjenar.site/2020/04/what-happen-funny-lol-videos.html",
+"https://www.clickjenar.site/2020/10/tips-ampuh-mengatasi-bruntusan-di-dahi.html",
+"https://www.clickjenar.site/2020/10/wow-3-manfaat-menakjubkan-memelihara.html",
+"https://www.clickjenar.site/2020/10/lakukan-hal-sepele-ini-agar-pasanganmu.html",
+"https://www.clickjenar.site/2020/10/ciri-ciri-perempuan-matre-yang-harus.html",
+"https://www.clickjenar.site/2020/10/banyak-penelitian-suami-tampan.html",
+"https://www.clickjenar.site/2020/10/cara-mengatasi-jerawat-batu-dengan-mudah.html",
+"https://www.clickjenar.site/2020/10/ini-5-manfaat-cabai-rawit-untuk.html",
+"https://www.clickjenar.site/2020/10/pasangan-tidak-percaya-padamu-seperti.html",
+"https://www.clickjenar.site/2020/10/5-hal-sepele-yang-menandakan-hubunganmu.html",
+"https://www.clickjenar.site/2020/10/manfaat-buah-pir-untuk-kecantikan-dan.html",
+"https://www.clickjenar.site/2020/10/tips-agar-kerutan-di-wajah-tidak.html",
+"https://www.clickjenar.site/2020/10/tips-make-up-untuk-wajah-berminyak-agar.html",
+"https://www.clickjenar.site/2020/10/hati-hati-dengan-makanan-ini-karena.html",
+"https://www.clickjenar.site/2020/10/5-tanda-dia-bukan-jodohmu-dan-harus.html",
+"https://www.clickjenar.site/2020/10/jika-mengalami-tanda-ini-lebih-baik.html",
+"https://www.clickjenar.site/2020/10/manfaat-memiliki-pasangan-cuek-yang.html",
+"https://www.clickjenar.site/2020/10/tidak-sembarangan-ini-telat-datang.html",
+
+
+];
+function randomPage() {
+return pages[Math.round(Math.random() * (pages.length - 1,2,3))];
+}
+location.href= randomPage();
