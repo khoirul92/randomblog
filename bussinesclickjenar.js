548c8726eae90c275d68dc976ff83a39a58d84b1
khoirul92 <54443082+khoirul92@users.noreply.github.com>
2020-11-04 22:22:26 +0700

    Create bussinesclickjenar.js

---
+++ bussinesclickjenar.js
@@ -0,0 +1,16 @@
+var pages = [
+"https://bussines.clickjenar.site/2020/10/business-planning-important-for.html",
+"https://bussines.clickjenar.site/2020/10/how-can-i-finance-small-extension-and.html",
+"https://bussines.clickjenar.site/2020/10/blink-blue-cross-partner-for-travel.html",
+"https://bussines.clickjenar.site/2020/10/airline-travellers-demand-rights-and.html",
+"https://bussines.clickjenar.site/2020/10/insurers-recognized-as-some-of-atlantic.html",
+"https://bussines.clickjenar.site/2020/10/axa-sells-ukraine-insurance-business-to.html",
+"https://bussines.clickjenar.site/2020/10/car-insurance-shock-this-common-error.html",
+"https://bussines.clickjenar.site/2020/10/it-just-got-much-cheaper-to-visit-north.html",
+
+
+];
+function randomPage() {
+return pages[Math.round(Math.random() * (pages.length - 1,2,3))];
+}
+location.href= randomPage();
