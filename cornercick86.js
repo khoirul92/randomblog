1011131a1bd69a0ad9a52405a4072209757fcf66
khoirul92 <54443082+khoirul92@users.noreply.github.com>
2020-11-08 20:03:59 +0700

    Create cornercick86.js

---
+++ cornercick86.js
@@ -0,0 +1,35 @@
+var pages = [
+"https://cornerkick86.blogspot.com/2020/11/man-city-vs-liverpool-ederson-hadapi.html",
+"https://cornerkick86.blogspot.com/2020/11/gol-kedua-bruno-fernandes-boleh-buat.html",
+"https://cornerkick86.blogspot.com/2020/10/layani-lionel-messi-bek-anyar-barcelona.html",
+"https://cornerkick86.blogspot.com/2020/10/jumpa-di-grup-g-liga-champions-lionel.html",
+"https://cornerkick86.blogspot.com/2020/09/3-pemain-yang-bisa-bawa-arsenal-kembali.html",
+"https://cornerkick86.blogspot.com/2020/09/3-pemain-baru-yang-bisa-bantu.html",
+"https://cornerkick86.blogspot.com/2020/09/skuad-juventus-bersenang-senang-dengan.html",
+"https://cornerkick86.blogspot.com/2018/10/tanpa-disangka-lawan-indonesia-di-8.html",
+"https://cornerkick86.blogspot.com/2018/10/tolak-tawaran-chelsea-karena-ronaldo.html",
+"https://cornerkick86.blogspot.com/2018/10/bikin-geger-ungkapan-presiden-juventus.html",
+"https://cornerkick86.blogspot.com/2018/10/terungkap-florentino-perez-akui-ini.html",
+"https://cornerkick86.blogspot.com/2018/10/luar-biasa-ronaldo-buat-sejarah-baru.html",
+"https://cornerkick86.blogspot.com/2018/10/real-madrid-menyesal-menjual-cristiano.html",
+"https://cornerkick86.blogspot.com/2018/10/juve-tutup-pintu-untuk-eks-mega-bintang.html",
+"https://cornerkick86.blogspot.com/2018/10/semakin-berkuasa-ronaldo-bawa-juventus.html",
+"https://cornerkick86.blogspot.com/2018/10/harga-pasarannya-anjlok-dan-sebulan.html",
+"https://cornerkick86.blogspot.com/2018/10/krisis-gol-dan-kemenangan-ramos.html",
+"https://cornerkick86.blogspot.com/2018/10/cetak-gol-lagi-cr7-bersaing-di-daftar.html",
+"https://cornerkick86.blogspot.com/2018/10/terlibat-kerusuhan-khabib-terancam.html",
+"https://cornerkick86.blogspot.com/2018/10/presiden-ufc-buka-peluang-copot-gelar.html",
+"https://cornerkick86.blogspot.com/2018/10/mengherankan-kok-bisanya-madrid-berani.html",
+"https://cornerkick86.blogspot.com/2018/10/bikin-kaget-pemain-barca-pencetak-dua.html",
+"https://cornerkick86.blogspot.com/2018/10/tak-berguna-sergio-ramos-minta-madrid.html",
+"https://cornerkick86.blogspot.com/2018/10/permainan-tak-kunjung-apiak-alexis.html",
+"https://cornerkick86.blogspot.com/2018/10/anfield-kuburan-bagi-manchester-city.html",
+
+
+
+
+];
+function randomPage() {
+return pages[Math.round(Math.random() * (pages.length - 1,2,3))];
+}
+location.href= randomPage();
