268c798fc571dff5a6f1369a4ad0c62edd2454cf
khoirul92 <54443082+khoirul92@users.noreply.github.com>
2020-11-23 20:22:20 +0700

    Create floting.js

---
+++ floting.js
@@ -0,0 +1,15 @@
+<div id="serdadu2" style="width:autopx;height:0;text-align:left;display:scroll;position:fixed;top:180px;left:15%">
+<div>
+    <script>
+        function myfunction() {
+     document.getElementById('serdadu2').style.display = 'none';
+ }
+var delay = 3700;
+
+    </script>
+<ahref="#" id="close-teaser" onclick="setTimeout(myfunction, delay);">
+<center><font color='red'><b><center class="close">&#215;</center></b></font></center></ahref="#"></div>
+<div id="Syanam-Abdillah" i-amphtml-fixedid="F0" style="top: 200px;"><b><center>= [ LIKE PAGE & CLOSE (X) ] To Read Article =</center></b>
+<iframe src="//www.facebook.com/plugins/likebox.php?href=https://web.facebook.com/Info-Tips-Asik-102383101686203
+ref=hl&amp;width=285&amp;height=240&amp;colorscheme=light&amp;show_faces=true&amp;border_color&amp;stream=false&amp;header=false&amp;appId=371524026202784" scrolling="no" frameborder="0"style="border:none; overflow:hidden; width:285px; height:240px;" allowtransparency="true"></iframe></div>
+</div>
